20732940fdc38757df3a97a0f4d0e12b31535b1d
cahinosilvadonascimento.yancarlos.studente@itispaleocapa.it <cahinosilvadonascimento.yancarlos.studente@itispaleocapa.it>
2024-01-11 10:47:30 +0100

    Per tipologie di domande utilizzo bool, ogni bool verifica il tipo di domanda

--- QuizTest_Cahino/Domande.cs
+++ QuizTest_Cahino/Domande.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing.Text;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace QuizTest_Cahino
 {
-    internal class Domande
+    public abstract class Domande
     {
+        public virtual string tipo()
+        {
+            Random rnd = new Random(); //valore rnd per quantità domande e tipologia
 
+
+        } 
     }
+    public class Tipo_Domande
+    {
+        public Tipo_Domande()
+        {
+            
+        }
+        public virtual void tipo1(bool ap_chiuso )
+        {
+            if(ap_chiuso )
+            {
+
+            }
+        }
+    }
+    
 }
